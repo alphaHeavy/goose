3c18f57c18a3247d66e3d0df4d0764429e7b92f9
Steve Severance <sseverance@alphaheavy.com>
2017-04-08 10:09:10 -0700

    Adding Additional date formats

--- src/main/scala/com/gravity/goose/extractors/PublishDateExtractor.scala
+++ src/main/scala/com/gravity/goose/extractors/PublishDateExtractor.scala
@@ -75,9 +75,13 @@ object PublishDateExtractor extends Logging {
       val date3 = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mmZ")
       val date4 = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
       val date5 = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss:Z")
+      val date6 = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mmz")
+      val date7 = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss z")
+      val date8 = DateTimeFormat.forPattern("yyyy-MM-ddzHH:mm:ssZ")
+      val date9 = DateTimeFormat.forPattern("E',' dd M YYYY HH:mm:ss Z")
       val parser = new DateTimeFormatterBuilder().append(null, Array(iso8601format1.getParser,
         iso8601format2.getParser,iso8601format3.getParser, date1.getParser, date2.getParser, date3.getParser,
-        date4.getParser,date5.getParser)).toFormatter
+        date4.getParser,date5.getParser,date6.getParser,date7.getParser,date8.getParser, date9.getParser)).toFormatter
       Option(parser.parseDateTime(txt)).map(x=>x.toDate)
     } catch {
       case ex: Exception =>
